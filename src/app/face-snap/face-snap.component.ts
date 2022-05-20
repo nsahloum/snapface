ddada3153598de6ed217a2c12d511d88f714a1c8
nsahloum <sahloumi.najima@gmail.com>
2022-05-20 17:49:30 +0200

    Onsnap

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,12 +11,26 @@ export class FaceSnapComponent implements OnInit {
   createdDate!: Date;
   snaps!: number;
   imageUrl!: string;
+  hasSnapped!: boolean;
+  buttonText!: string;
 
   ngOnInit() {
     this.title = 'Hello';
     this.description ='Hello World!';
     this.createdDate = new Date();
     this.snaps = 6;
-    this.imageUrl = 'https://dummyimage.com/600x400/000/fff'
+    this.imageUrl = 'https://dummyimage.com/600x400/000/fff';
+    this.hasSnapped = false;
+    this.buttonText = "Oh snap!"
+  }
+
+  onSnap() {
+    if (this.buttonText === "Oh snap!"){
+      this.snaps++;
+      this.buttonText = "Unsnap!";
+    } else {
+      this.snaps--;
+      this.buttonText = "Oh snap!";
+    }
   }
 }
