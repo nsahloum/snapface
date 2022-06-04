23195b39dcc101ae4d918b2820c876d60f76e311
nsahloum <sahloumi.najima@gmail.com>
2022-06-04 17:38:28 +0200

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,54 @@ import {FaceSnap} from "./models/face-snap.model";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  mySnap!: FaceSnap;
-  mySecondSnap!: FaceSnap;
-  myThirdSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.mySnap = {
-      title: 'Hello',
-      description: 'Hello World',
-      createdDate: new Date(),
-      snaps: 12,
-      imageUrl: 'https://dummyimage.com/600x400/000/fff',
-    };
-
-    this.mySecondSnap = {
-      title: 'Hello2',
-      description: 'Hello World2',
-      createdDate: new Date(),
-      snaps: 1,
-      imageUrl: 'https://dummyimage.com/600x400/fff/000',
-    };
-
-    this.myThirdSnap = {
-      title: 'Hello3',
-      description: 'Hello World3',
-      createdDate: new Date(),
-      snaps: 2,
-      imageUrl: 'https://dummyimage.com/600x400/cf1fcf/fff',
-    };
-
+    this.faceSnaps = [
+      {
+        title: 'Hello',
+        description: 'Hello World',
+        createdDate: new Date(),
+        snaps: 12,
+        imageUrl: 'https://dummyimage.com/600x400/000/fff',
+        location: 'Brussels'
+      },
+      {
+        title: 'Hello2',
+        description: 'Hello World2',
+        createdDate: new Date(),
+        snaps: 1,
+        imageUrl: 'https://dummyimage.com/600x400/fff/000',
+      },
+      {
+        title: 'Hello3',
+        description: 'Hello World3',
+        createdDate: new Date(),
+        snaps: 2,
+        imageUrl: 'https://dummyimage.com/600x400/cf23cf/000',
+      },
+      {
+        title: 'Hello',
+        description: 'Hello World',
+        createdDate: new Date(),
+        snaps: 12,
+        imageUrl: 'https://dummyimage.com/600x400/000/fff',
+        location: 'Brussels'
+      },
+      {
+        title: 'Hello2',
+        description: 'Hello World2',
+        createdDate: new Date(),
+        snaps: 1,
+        imageUrl: 'https://dummyimage.com/600x400/fff/000',
+      },
+      {
+        title: 'Hello3',
+        description: 'Hello World3',
+        createdDate: new Date(),
+        snaps: 2,
+        imageUrl: 'https://dummyimage.com/600x400/cf23cf/000',
+      },
+    ]
   }
 }
