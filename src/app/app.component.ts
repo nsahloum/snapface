679200d9e979b65aeb88b33c153baeead4179c0c
nsahloum <sahloumi.najima@gmail.com>
2022-06-04 17:14:24 +0200

    better declaration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,33 @@ import {FaceSnap} from "./models/face-snap.model";
 })
 export class AppComponent implements OnInit{
   mySnap!: FaceSnap;
+  mySecondSnap!: FaceSnap;
+  myThirdSnap!: FaceSnap;
 
   ngOnInit() {
-    this.mySnap = new FaceSnap(
-      'Hello',
-      'Hello World',
-      new Date(),
-      12,
-      'https://dummyimage.com/600x400/000/fff',
-    );
-  }
+    this.mySnap = {
+      title: 'Hello',
+      description: 'Hello World',
+      createdDate: new Date(),
+      snaps: 12,
+      imageUrl: 'https://dummyimage.com/600x400/000/fff',
+    };
 
+    this.mySecondSnap = {
+      title: 'Hello2',
+      description: 'Hello World2',
+      createdDate: new Date(),
+      snaps: 1,
+      imageUrl: 'https://dummyimage.com/600x400/fff/000',
+    };
+
+    this.myThirdSnap = {
+      title: 'Hello3',
+      description: 'Hello World3',
+      createdDate: new Date(),
+      snaps: 2,
+      imageUrl: 'https://dummyimage.com/600x400/cf1fcf/fff',
+    };
+
+  }
 }
